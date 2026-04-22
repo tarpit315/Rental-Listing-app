82fee118ad26215d3cc631146fa714b39d85ce89
tarpit <tarpit315@gmail.com>
2026-04-23 01:21:35 +0530

    check valid data

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine" , "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+
 app.engine('ejs',ejsMate);
 
 
